a58799daaeba3078163d4049022706b56a5388fa
Priyanshi dhakad <prayanshudhakad405@gmail.com>
2025-01-04 08:34:01 +0530

    Update first.js

--- first.js
+++ first.js
@@ -3,7 +3,7 @@ let qrImage=document.getElementById("qrImage");
 let qrText=document.getElementById("qrText");
 
 function generateQR(){
-if(qrText.value.lenght>0){
+if(qrText.value.length>0){
     qrImage.src="https://api.qrserver.com/v1/create-qr-code/?=150x150&data="+qrText.value;
    imgBox.classList.add("show-img");
    }
